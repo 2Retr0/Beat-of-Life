3dc12aa76122cbca87ff6d871b1ea383d02c1e04
Ethan Truong <18603664+2Retr0@users.noreply.github.com>
2025-01-12 15:37:20 -0800

    Basic camera track + some props
    Also added an addon for previewing camera picture-in-picture.

--- level/demo/demo.gd
+++ level/demo/demo.gd
@@ -8,7 +8,7 @@ class_name Demo extends Node
 		var path = '%s.osu' % track.resource_path.trim_suffix('.%s' % track.resource_path.get_file().get_extension())
 		print(path)
 		if not FileAccess.file_exists(path): return
-		
+
 		var beatmap = ManiaParser.load_beatmap(path)
 		beatmap.beatmap_set.track = track
 		self.beatmap = beatmap
@@ -17,7 +17,10 @@ class_name Demo extends Node
 
 func _ready() -> void:
 	var player = beatmap.ruleset.create_player(beatmap)
-	
+
 	add_child(player)
-	
+
 	player.play()
+
+	var tween := get_tree().create_tween()
+	tween.tween_property($CameraTrack/Path, ^'progress_ratio', 1.0, 7.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
